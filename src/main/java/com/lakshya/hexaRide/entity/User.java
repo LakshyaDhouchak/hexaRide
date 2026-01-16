867e78c84ecb004a6c3100bc42d6a63dd82854e2
lakshya <lakshya10171@gmail.com>
2026-01-16 07:10:09 +0530

    implementation of user entity

---
+++ src/main/java/com/lakshya/hexaRide/entity/User.java
@@ -0,0 +1,54 @@
+package com.lakshya.hexaRide.entity;
+
+import java.time.LocalDateTime;
+
+import com.lakshya.hexaRide.enums.Role;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name ="users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User {
+    // define user entity attributes here
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private Double rating;
+
+    private LocalDateTime createdAt = LocalDateTime.now();
+    
+}
