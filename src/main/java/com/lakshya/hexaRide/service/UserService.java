8bb327d6d1a42d3de361b1fe38cb9ad7bc324ed4
lakshya <lakshya10171@gmail.com>
2026-01-17 09:51:54 +0530

    add userService interface

--- src/main/java/com/lakshya/hexaRide/service/UserService.java
+++ src/main/java/com/lakshya/hexaRide/service/UserService.java
@@ -1,5 +1,17 @@
 package com.lakshya.hexaRide.service;
 
-public class UserService {
+import java.util.List;
+
+import com.lakshya.hexaRide.dto.UserCreateDTO;
+import com.lakshya.hexaRide.dto.UserResponseDTO;
+import com.lakshya.hexaRide.dto.UserUpdateDTO;
+
+public interface UserService {
+    // define the methord
+    UserResponseDTO signUp(UserCreateDTO userDTO);
+    UserResponseDTO getProfile(Long id);
+    UserResponseDTO updateProfile(Long id , UserUpdateDTO userDTO);
+    void deleteUser(Long id);
+    List<UserResponseDTO> getAllUsers();
     
-}
+} 
