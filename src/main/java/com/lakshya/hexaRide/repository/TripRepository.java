ba6174c4d0bc117a02e14b0b8945f7134abe7ecc
lakshya <lakshya10171@gmail.com>
2026-01-17 22:23:18 +0530

    add Trip Repo updated

--- src/main/java/com/lakshya/hexaRide/repository/TripRepository.java
+++ src/main/java/com/lakshya/hexaRide/repository/TripRepository.java
@@ -1,11 +1,17 @@
 package com.lakshya.hexaRide.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.lakshya.hexaRide.entity.Trip;
+import com.lakshya.hexaRide.entity.User;
+import com.lakshya.hexaRide.enums.TripStatus;
 
 @Repository
 public interface TripRepository extends JpaRepository<Trip,Long> {
-
+    // define the methord
+    boolean existsByRiderAndStatusIn(User rider, List<TripStatus> statuses);
+    boolean existsByDriverAndStatusIn(User driver, List<TripStatus> statuses);
 }
