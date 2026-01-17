ad3c464c8f87a4101c55a214b3935fcf4c9b44ee
lakshya <lakshya10171@gmail.com>
2026-01-17 22:23:55 +0530

    add trip Service interface

---
+++ src/main/java/com/lakshya/hexaRide/service/TripServiceImpl.java
@@ -0,0 +1,143 @@
+package com.lakshya.hexaRide.service;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lakshya.hexaRide.dto.TripRequestDTO;
+import com.lakshya.hexaRide.dto.TripResponseDTO;
+import com.lakshya.hexaRide.entity.Trip;
+import com.lakshya.hexaRide.entity.User;
+import com.lakshya.hexaRide.enums.TripStatus;
+import com.lakshya.hexaRide.exceptionHandling.InvalidCredentialException;
+import com.lakshya.hexaRide.exceptionHandling.ResourceNotFoundException;
+import com.lakshya.hexaRide.repository.TripRepository;
+import com.lakshya.hexaRide.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class TripServiceImpl implements TripService {
+    // define the properties
+    private final UserRepository userRepository;
+    private final TripRepository tripRepository;
+    private final UserService userService;
+    private TripResponseDTO mapToResponseDTO(Trip trip){
+        return TripResponseDTO.builder()
+                .id(trip.getId())
+                .status(trip.getStatus())
+                .pickupLat(trip.getPickupLat())
+                .pickupLng(trip.getPickupLng())
+                .dropoffLat(trip.getDropoffLat())
+                .dropoffLng(trip.getDropoffLng())
+                .completedAt(trip.getCompletedAt())
+                .distanceKm(trip.getDistanceKm())
+                .fare(trip.getFare())
+                .startedAt(trip.getStartedAt())
+                .driver(trip.getDriver()!=null ? userService.mapToResponseDTO(trip.getDriver()) : null)
+                .rider(userService.mapToResponseDTO(trip.getDriver()))
+                .build();
+    }
+    private final List<TripStatus> activeStatuses = Arrays.asList(
+            TripStatus.REQUESTED, TripStatus.ACCEPTED, TripStatus.STARTED
+    );
+
+    @Transactional
+    @Override
+    public TripResponseDTO requestTrip(TripRequestDTO tripDTO) {
+        // define the condition
+        User rider = userRepository.findById(tripDTO.getRiderId())
+                    .orElseThrow(()-> new ResourceNotFoundException("Rider not found with id: " + tripDTO.getRiderId()));
+        if(tripRepository.existsByRiderAndStatusIn(rider,activeStatuses)){
+            throw new InvalidCredentialException("You already have an active or pending ride request.");
+        }            
+
+        Trip trip = Trip.builder()
+                    .rider(rider)
+                    .dropoffLat(tripDTO.getDropoffLat())
+                    .dropoffLng(tripDTO.getDropoffLng())
+                    .pickupLat(tripDTO.getPickupLat())
+                    .pickupLng(tripDTO.getPickupLng())
+                    .fare(tripDTO.getEstimatedFare())
+                    .status(TripStatus.REQUESTED)
+                    .build();
+        Trip savedTrip = tripRepository.save(trip);
+        return mapToResponseDTO(savedTrip);                      
+    }
+
+    @Transactional
+    @Override
+    public TripResponseDTO acceptTrip(Long tripId, Long driverId) {
+        Trip trip = findTripById(tripId);
+        // define the condition
+        if(trip.getStatus() != TripStatus.REQUESTED){
+            throw new InvalidCredentialException("This trip is no longer available.");
+        }
+        User driver = userRepository.findById(driverId)
+                .orElseThrow(() -> new ResourceNotFoundException("Driver not found"));
+        if (tripRepository.existsByDriverAndStatusIn(driver, activeStatuses)) {
+            throw new InvalidCredentialException("Driver is already on another active trip.");
+        }
+
+        trip.setDriver(driver);
+        trip.setStatus(TripStatus.ACCEPTED);
+        return mapToResponseDTO(tripRepository.save(trip));        
+    }
+
+    @Transactional
+    @Override
+    public TripResponseDTO startTrip(Long tripId) {
+        // define the condition
+        Trip trip = findTripById(tripId);
+        if(trip.getStatus() != TripStatus.ACCEPTED){
+            throw new InvalidCredentialException("Trip cannot start until it is accepted.");
+        }
+        trip.setStatus(TripStatus.STARTED);
+        trip.setCompletedAt(LocalDateTime.now());
+        return mapToResponseDTO(trip);
+    }
+
+    @Transactional
+    @Override
+    public TripResponseDTO completeTrip(Long tripId) {
+        // define the condition
+        Trip trip = findTripById(tripId);
+        if(trip.getStatus() != TripStatus.IN_PROGRESS){
+            throw new InvalidCredentialException("Cannot complete a trip that hasn't started.");
+        }
+        trip.setStatus(TripStatus.COMPLETED);
+        trip.setCompletedAt(LocalDateTime.now());
+        return mapToResponseDTO(trip);
+    }
+
+    @Transactional
+    @Override
+    public TripResponseDTO cancelTrip(Long tripId) {
+        // define the condition
+        Trip trip = findTripById(tripId);
+        if(trip.getStatus() == TripStatus.COMPLETED || trip.getStatus() == TripStatus.CANCELLED){
+            throw new InvalidCredentialException("Trip cannot be cancelled from its current state: " + trip.getStatus());
+        }
+        trip.setStatus(TripStatus.CANCELLED);
+        return mapToResponseDTO(trip);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public TripResponseDTO getTripById(Long tripId) {
+        Trip trip = tripRepository.findById(tripId)
+                    .orElseThrow(()-> new ResourceNotFoundException("Trip not found with id: " + tripId));
+        return mapToResponseDTO(trip);            
+    }
+
+    // define the helper class
+    private Trip findTripById(Long id){
+        return tripRepository.findById(id)
+            .orElseThrow(()-> new ResourceNotFoundException("Trip not found with id: " + id));
+    }
+    
+}
