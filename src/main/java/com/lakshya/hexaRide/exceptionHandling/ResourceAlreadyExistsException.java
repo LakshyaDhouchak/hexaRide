6ff0b8c2aee1940c0e89e8d59998725ed7455bb8
lakshya <lakshya10171@gmail.com>
2026-01-17 09:51:30 +0530

    add ResourceAlreadyExistsException

---
+++ src/main/java/com/lakshya/hexaRide/exceptionHandling/ResourceAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.lakshya.hexaRide.exceptionHandling;
+
+public class ResourceAlreadyExistsException extends HexaRideException {
+    // define the constructor
+    public ResourceAlreadyExistsException(String message){
+        super(message);
+    } 
+}
