cb121e9c967c352563535d454f7e69423a7d9773
lakshya <lakshya10171@gmail.com>
2026-01-17 09:49:49 +0530

    add HexaRide Exception Handler

---
+++ src/main/java/com/lakshya/hexaRide/exceptionHandling/HexaRideException.java
@@ -0,0 +1,8 @@
+package com.lakshya.hexaRide.exceptionHandling;
+
+public class HexaRideException extends RuntimeException {
+    // define the constructor
+    public HexaRideException(String message){
+        super(message);
+    }
+}
