b4edcaaf0f0034e94e86420a9b64bb0484c7f62d
lakshya <lakshya10171@gmail.com>
2026-01-17 18:48:53 +0530

    implement VehicleRepository

--- src/main/java/com/lakshya/hexaRide/repository/VehicleRepository.java
+++ src/main/java/com/lakshya/hexaRide/repository/VehicleRepository.java
@@ -1,5 +1,7 @@
 package com.lakshya.hexaRide.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,6 @@ import com.lakshya.hexaRide.entity.Vehicle;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle ,Long> {
-
+    Optional<Vehicle> findByDriverId(Long id);
     
 }
